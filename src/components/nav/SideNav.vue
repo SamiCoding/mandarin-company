41e181127a224de90396472f22ecf4b86fdf6018
SamiCoding <samicoding@gmail.com>
2024-03-30 19:08:33 +0900

    [Add] 사이드 네비게이션바 css 추가

--- src/components/nav/SideNav.vue
+++ src/components/nav/SideNav.vue
@@ -1,9 +1,10 @@
 <template>
   <nav>
-    <div>{{ sideMenu[0] }}</div>
-    <ul>
+    <div class="side-nav__main">{{ sideMenu[0] }}</div>
+    <ul class="side-nav__sub">
       <li v-for="(sideSubMenu, i) in sideSubMenuList" :key="i">
         <router-link
+          :class="{'side-nav__selected' : menuQuery===sideSubMenu[1]}"
           :to="{
             name: `${sideMenu[1]}Page`,
             params: {str: `${sideSubMenu[1]}`},
@@ -45,6 +46,46 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
 
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
+  .side-nav__main {
+    padding: 50px 0 10px 10px;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+    background-color: rgba(0, 100, 255, 0.6);
+    font-family: 'NanumSquareExtraBold';
+    font-size: 18px;
+  }
+
+  .side-nav__sub {
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    background-color: rgba(255, 255, 0, 0.3);
+    font-family: 'NanumSquareBold';
+  }
+
+  .side-nav__sub > li {
+    border-bottom: 1px solid black;
+  }
+
+  .side-nav__sub > li > a {
+    display: inline-block;
+    width: 100%;
+    padding: 5px 15px;
+  }
+
+  .side-nav__selected {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
 </style>
--- src/layouts/SideNavLayout.vue
+++ src/layouts/SideNavLayout.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <SideNav/>
-    <slot></slot>
+    <div class="side-image">
+      <div class="side-image__img"></div>
+    </div>
+    <div class="side-layout">
+      <SideNav id="side-nav"/>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -15,6 +20,51 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .side-image {
+    height: 150px;
+    background: none;
+  }
 
+  .side-image__img {
+    height: 100%;
+    background-image: url("../../public/images/main_img.jpg");
+    background-size: cover;
+  }
+
+  .side-layout {
+    display: flex;
+    margin: 0 10vw;
+  }
+
+  #side-nav {
+    min-width: 160px;
+  }
+
+  @media (max-width: 1000px) {
+    .side-layout {
+      margin: 0 5vw;
+    }
+  }
+
+  /* mobile */
+  @media (max-width: 700px) {
+    .side-image {
+      height: 100px;
+    }
+
+    .side-layout {
+      margin: 0;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .side-image {
+      height: 50px;
+    }
+
+    #side-nav {
+      display: none;
+    }
+  }
 </style>
