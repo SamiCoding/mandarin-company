4176d98eb4533fb900ec6b18c8d12b9205706926
SamiCoding <samicoding@gmail.com>
2024-03-30 16:02:00 +0900

    [Add] 네비게이션바 url 경로 이동 구현

--- src/components/nav/MainNav.vue
+++ src/components/nav/MainNav.vue
@@ -1,16 +1,36 @@
 <template>
   <nav>
     <div>
-      <v-img src="../../../public/images/귤컴퍼니_logo.png" alt="Logo" width="60px"/>
-      <span>귤컴퍼니</span>
+      <router-link to="/">
+        <v-img src="../../../public/images/귤컴퍼니_logo.png" alt="Logo" width="60px"/>
+        <span>귤컴퍼니</span>
+      </router-link>
     </div>
     <div>
       <ul>
         <li v-for="(navMenu, i) in navMenuList" :key="i">
-          <span>{{ navMenu[0] }}</span>
+          <span>
+            <router-link
+              :to="{
+                name: `${navMenu[1]}Page`,
+                params: {str: `${navSubMenuList[i][0][1]}`},
+              }"
+            >
+              {{ navMenu[0] }}
+            </router-link>
+          </span>
           <ul>
             <li v-for="(navSubMenu, j) in navSubMenuList[i]" :key="j">
-              <span>{{ navSubMenu[0] }}</span>
+              <span>
+                <router-link
+                  :to="{
+                    name: `${navMenu[1]}Page`,
+                    params: {str: `${navSubMenu[1]}`},
+                  }"
+                >
+                  {{ navSubMenu[0] }}
+                </router-link>
+              </span>
             </li>
           </ul>
         </li>
