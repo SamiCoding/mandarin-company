11744e57e950e33d2ce1e4ca21e29174ea8838df
SamiCoding <samicoding@gmail.com>
2024-03-30 16:22:14 +0900

    [Add] 푸터에 내용 표시

--- src/components/nav/FooterNav.vue
+++ src/components/nav/FooterNav.vue
@@ -1,5 +1,21 @@
 <template>
-  Footer
+  <div>
+    <ul>
+      <li>개인정보 처리방침</li>
+      <li>이메일 무단수집거부</li>
+    </ul>
+    <ul>
+      <li>상호 : 귤컴퍼니</li>
+      <li>대표 : 김귤</li>
+      <li>전화 : 010-1234-5678</li>
+      <li>팩스 : 12-345-6789</li>
+      <li>사업자 등록번호 : 012-34-56789</li>
+      <li>주소 : 광주광역시 서구</li>
+    </ul>
+    <ul>
+      <li>Copyright ⓒ 2024 귤컴퍼니. All Rights Reserved.</li>
+    </ul>
+  </div>
 </template>
 
 <script>
