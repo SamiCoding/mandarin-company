752a8aab741216ff2eeec90f7b5a06b82ccebe83
SamiCoding <samicoding@gmail.com>
2024-03-30 15:49:09 +0900

    [Add] 네비게이션바에 메뉴 표시

--- src/components/nav/MainNav.vue
+++ src/components/nav/MainNav.vue
@@ -1,10 +1,35 @@
 <template>
-  Main
+  <nav>
+    <div>
+      <v-img src="../../../public/images/귤컴퍼니_logo.png" alt="Logo" width="60px"/>
+      <span>귤컴퍼니</span>
+    </div>
+    <div>
+      <ul>
+        <li v-for="(navMenu, i) in navMenuList" :key="i">
+          <span>{{ navMenu[0] }}</span>
+          <ul>
+            <li v-for="(navSubMenu, j) in navSubMenuList[i]" :key="j">
+              <span>{{ navSubMenu[0] }}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    navMenuList: [],
+    navSubMenuList: [],
+  }),
 
+  mounted() {
+    this.navMenuList = this.$navMenuList
+    this.navSubMenuList = this.$navSubMenuList
+  },
 }
 </script>
 
