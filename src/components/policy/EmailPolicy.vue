260d2a6e6a3256aa096481a027ee50488cf7b295
SamiCoding <samicoding@gmail.com>
2024-03-30 17:33:47 +0900

    [Add] 모달창 구현

---
+++ src/components/policy/EmailPolicy.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog max-width="800">
+    <template v-slot:activator="{ props: activatorProps }">
+      <b v-bind="activatorProps">{{ title }}</b>
+    </template>
+    <template v-slot:default="{ isActive }">
+      <v-card>
+        <div>
+          <v-icon icon="mdi-close" @click="isActive.value=false"/>
+        </div>
+        <div class="policy__title">
+          <h2>{{ title }}</h2>
+        </div>
+        <div>
+          <div>
+            Content
+          </div>
+        </div>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: '이메일 무단수집거부'
+  }),
+}
+</script>
+
+<style>
+
+</style>
