fc1df1cc334a8e3053012f1b5acc894b83f2f33d
SamiCoding <samicoding@gmail.com>
2024-03-30 15:37:03 +0900

    [Add] main.js에 전역변수 파일 등록하기

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
+import nav from './plugins/nav'
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .use(router)
+  .use(nav)
   .mount('#app')
