53866c550606e52ea6f65011d81d31d23ee1a359
SamiCoding <samicoding@gmail.com>
2024-03-30 16:46:47 +0900

    [Add] 사이드 네비게이션바에 메뉴 표시

--- src/components/nav/SideNav.vue
+++ src/components/nav/SideNav.vue
@@ -1,10 +1,29 @@
 <template>
-  Side
+  <nav>
+    <div>{{ sideMenu[0] }}</div>
+    <ul>
+      <li v-for="(sideSubMenu, i) in sideSubMenuList" :key="i">
+        {{ sideSubMenu[0] }}
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    sideMenu: [],
+    sideSubMenuList: [],
+  }),
 
+  mounted() {
+    const sideNav = this.$sideNav()
+    
+    if (sideNav.correctPath) {
+      this.sideMenu = sideNav.sideMenu
+      this.sideSubMenuList = sideNav.sideSubMenuList
+    }
+  },
 }
 </script>
 
