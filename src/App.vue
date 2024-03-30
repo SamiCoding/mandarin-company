faaf64cc81ae18d03940547173cbb2fa2a468b76
SamiCoding <samicoding@gmail.com>
2024-03-30 14:08:16 +0900

    [Add] App.vue에 MainNav와 Footer 추가

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <v-app>
     <v-main>
+      <MainNav/>
       <component id="content" :is="$route.meta.layout || 'div'">
         <router-view/>
       </component>
+      <FooterNav/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import MainNav from '@/components/nav/MainNav.vue'
+import FooterNav from '@/components/nav/FooterNav.vue'
 
 export default {
   name: 'App',
 
   components: {
-    
+    MainNav,
+    FooterNav,
   },
 
   data: () => ({
